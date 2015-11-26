ab65ecdb6072f9062927725ece3b3b0f8cd33a86
walchko <kevin.walchko@outlook.com>
2015-11-26 15:42:22 -0700

    working

--- bin/server.js
+++ bin/server.js
@@ -3,22 +3,41 @@
 var debug = require('debug')('kevin:mon')
 //var chalk = require('chalk');
 var program = require('commander');
-
+var os = require('os');
 var http = require('http');                     // http-server
 
 var sysinfo = require('../lib/sysinfo.js');
 var makePage = require('../lib/page.js');
+var realtime = require('../lib/realtime.js');
 
 // var spawn = require('child_process').exec;
 // var ls = spawn('ls -alh');
 
 var pck = require('../package.json');
 
+debug('argv: '+process.argv);
+
 program
 	.version(pck.version)
+	.description(pck.description)
 	.usage(pck.name + ' [options]')
-	.option('-p, --port <port>','Http server port number',parseInt,8080)
+	.option('-p, --port <port>','Http server port number, default: 8080',parseInt,8080)
+	.option('-r, --no-static','Do real-time webpage updates')
+// 	.option('-t, --test','test')
 	.parse(process.argv);
+
+debug('!rt: '+ program.static);
+// debug('rt: ' + (program.realtime ? true : false) );
+// debug('test: '+ program.test);
+debug('port: '+ program.port);
+
+// program.port = 8080;
+
+// not sure real-time info is worth it on small RPi
+var rt = false;
+if(!program.static){
+	rt = true;
+}
 
 // var disk = require('diskusage');
 //  
@@ -32,9 +51,9 @@ program
 // Simple REST server
 server = http.createServer(function(req, res){
     var path = req.url; 
-    debug( path );
+    debug( 'path: ' + path );
     
-	var info = sysinfo();
+	var info = sysinfo.sysinfo();
 	
 	if ( path == '/json'){
 		if (req.method == 'GET') {
@@ -46,7 +65,7 @@ server = http.createServer(function(req, res){
 	else if ( path == '/' ){
 		if (req.method == 'GET') {
 			res.writeHead(200,{'Content-Type': 'text/html'});
-			res.write(makePage(info));
+			res.write(makePage(info,rt));
 			res.end();
 		}
 	}
@@ -59,6 +78,11 @@ server = http.createServer(function(req, res){
 	}
 });
 
+if (rt) {
+	realtime(server,1000);
+}
+
+// start web server
 server.listen(program.port);
 
 
