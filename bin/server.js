b612d1254c33c26ea21d84b1b93a2d531af160c0
walchko <kevin.walchko@outlook.com>
2015-11-28 19:51:53 -0700

    major update
    rt, qr, storage add

--- bin/server.js
+++ bin/server.js
@@ -1,21 +1,18 @@
 #!/usr/bin/env node
 
-var debug = require('debug')('kevin:mon')
-//var chalk = require('chalk');
-var program = require('commander');
-var os = require('os');
-var http = require('http');                     // http-server
+var debug = require('debug')('kevin:mon')  // debugging
+//var chalk = require('chalk');            // colors
+var program = require('commander');        // CLI access
+var os = require('os');                    // OS access
+var http = require('http');                // http-server
 
-var sysinfo = require('../lib/sysinfo.js');
-var makePage = require('../lib/page.js');
-var realtime = require('../lib/realtime.js');
+var sysinfo = require('../lib/sysinfo.js');   // get system info
+var makePage = require('../lib/page.js');     // create web page
+var realtime = require('../lib/realtime.js'); // update web page via socket.io
+var qr = require('../lib/qr.js');             // draw QR image w/ network info
 
-// var spawn = require('child_process').exec;
-// var ls = spawn('ls -alh');
-
+// grab info from npm package
 var pck = require('../package.json');
-
-debug('argv: '+process.argv);
 
 program
 	.version(pck.version)
@@ -23,15 +20,14 @@ program
 	.usage(pck.name + ' [options]')
 	.option('-p, --port <port>','Http server port number, default: 8080',parseInt,8080)
 	.option('-r, --no-static','Do real-time webpage updates')
-// 	.option('-t, --test','test')
+// 	.option('-u, --update','update time for real-time, default: 1000 msec', parseInt, 1000)
 	.parse(process.argv);
-
-debug('!rt: '+ program.static);
+	
+// debug('argv: '+process.argv);
+// debug('!rt: '+ program.static);
 // debug('rt: ' + (program.realtime ? true : false) );
 // debug('test: '+ program.test);
-debug('port: '+ program.port);
-
-// program.port = 8080;
+debug('archeyjs ready on port: '+ program.port);
 
 // not sure real-time info is worth it on small RPi
 var rt = false;
@@ -39,21 +35,13 @@ if(!program.static){
 	rt = true;
 }
 
-// var disk = require('diskusage');
-//  
-// // get disk usage. Takes mount point as first parameter 
-// disk.check('/', function(err, info) {
-//     console.log(info.available);
-//     console.log(info.free);
-//     console.log(info.total);
-// });
-
 // Simple REST server
 server = http.createServer(function(req, res){
     var path = req.url; 
     debug( 'path: ' + path );
     
 	var info = sysinfo.sysinfo();
+	var qrimage = qr(info);
 	
 	if ( path == '/json'){
 		if (req.method == 'GET') {
@@ -69,6 +57,10 @@ server = http.createServer(function(req, res){
 			res.end();
 		}
 	}
+	else if ( path === '/qr' ){
+        res.writeHead(200, {'Content-Type': 'image/png'});
+        qrimage.pipe(res);
+    }
 	else {
 		// force users to / or /json
 		debug("Wrong path " + path)
